a83b4ec7785233f3de6de08f8508e183e641e588
Daniel Jones <danieljones314@gmail.com>
2024-02-01 16:45:45 +0100

    Created useState hook that calls recipes for specific types of cuisine from the api.

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { Link, useParams } from "react-router-dom";
 
 function Cuisine() {
-  return <div>Cuisine</div>;
+  const [cuisine, setCuisine] = useState([]);
+  const getCuisine = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
+    );
+    const recipes = await data.json();
+    setCuisine(recipes.results);
+  };
+
+  return <div></div>;
 }
 
 export default Cuisine;
