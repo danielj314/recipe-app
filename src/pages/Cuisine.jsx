d4d4e0c45c68554d4687ae5a41cc483c81e56b6b
Daniel Jones <danieljones314@gmail.com>
2024-02-01 17:00:24 +0100

    Created useEffect hook that calls the getCuisine useState hook, depending on which parameters are selected.

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 import { Link, useParams } from "react-router-dom";
 
 function Cuisine() {
   const [cuisine, setCuisine] = useState([]);
+  let params = useParams();
   const getCuisine = async (name) => {
     const data = await fetch(
       `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
@@ -13,6 +14,9 @@ function Cuisine() {
     setCuisine(recipes.results);
   };
 
+  useEffect(() => {
+    getCuisine(params.type);
+  }, [params.type]);
   return <div></div>;
 }
 
